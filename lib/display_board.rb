cc2f31846d581769984e7b3dd4ad4f01fe08786d
Elena Dinca <elenadinca25@gmail.com>
2019-11-24 12:33:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,4 @@ def dispaly_board
   puts "Tic Tac Toe Board"
 end
 
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
